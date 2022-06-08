59ce08ade0b2a1e203f60fb60751590ab65cf0ef
ozngrsc <ozangursucu@hotmail.com>
2022-06-08 09:55:15 +0300

    widgets

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening?</h2>
+
+        <TwitterTweetEmbed tweetId={"1534073268684173313"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="OznGrsc_"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://www.facebook.com/ozan.gursucu"}
+          options={{ text: "#reactjs is awesome", via: "OznGrsc_" }}
+        />
+      </div>
     </div>
   );
 }
